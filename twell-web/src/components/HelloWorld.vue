a7e9be925a49dc3058c15ab3b3caced42e39835b
Jahnavi Gupta <guptajhnv@gmail.com>
2021-01-30 19:57:44 +0530

    chore: add code generator

---
+++ twell-web/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <body>
+    <div class="body">
+      <h1>{{ msg }}</h1>
+      <input v-model="name" placeholder="Full Name">
+      <div class="skip"></div>
+      <input v-model="date" placeholder="Date of visit (dd/mm/yyyy)">
+      <div class="skip"></div>
+      <input v-model="count" placeholder="Number of visitors">
+      <div class="skip"></div>
+      <button v-on:click="generateCode(name, date, count)">Generate QR Code</button>
+      <div class="skip"></div>
+      <div v-if="valid == true">
+        <qrcode-vue :value="value" :size="size" level="H" />
+      </div>    
+    </div>
+  </body>
+</template>
+
+<script>
+  import QrcodeVue from 'qrcode.vue'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      name: "",
+      date: "",
+      count: "",
+      value: "",
+      valid: false,
+    }
+  },
+  components: {
+    QrcodeVue,
+  },
+  methods: {
+    generateCode: function (name, date, count) {
+      this.valid = false
+      this.value = name + "_" + date + "_" + count
+      if (name.length > 0 && date.length > 0 && count.length > 0) {
+        this.valid = true
+        alert("Generate QR Code")
+        if (event) {
+          this.updateBookingList()
+        }
+      }
+      else {
+        alert("Enter details to generate QR Code")
+      }
+    },
+    updateBookingList: function (event) {
+      alert("Visitor list upated")
+    }
+
+  }
+}
+</script>
